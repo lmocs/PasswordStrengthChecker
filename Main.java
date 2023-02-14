f0991b7062b6ddb818c2504ced20b3272fa21f37
lmocs <109308151+lmocs@users.noreply.github.com>
2023-02-14 01:33:53 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,11 @@
 /*
-
-takes in string input from user
-cannot contain a space
-needs to be at least 8 chars long
-one lowercase and one uppercase
-one number
-one special char
-
+ Criteria: 
+ 1. Takes in string input from user
+ 2. Cannot contain a space
+ 3. Needs to be at least 8 chars long
+ 4. One lowercase and one uppercase
+ 5. One number
+ 6. One special char
 */
 
 import java.util.Scanner;
@@ -16,9 +15,11 @@ public class Main {
         Scanner scnr = new Scanner(System.in);
         StrengthCheck entry = new StrengthCheck();
 
+        // This prompts the user to enter a password to check
         System.out.println("Enter your password: ");
         String password = scnr.nextLine();
 
+        // This executes the method from the StrengthCheck class
         entry.check(password);
     }
 }
